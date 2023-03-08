23983afd6f726c34bd959ed630afc353f02b257a
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-08 23:58:22 +0100

    Add missing election slug view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,18 +1,27 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 import BallotTrackerView from "../views/BallotTrackerView.vue";
+import MissingSlugView from "../views/MissingSlugView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: "/",
+      redirect: to => "/en",
+    },
+    {
+      path: "/:locale",
+      component: MissingSlugView,
+    },
+    {
       name: "Welcome",
-      path: "/:electionSlug",
+      path: "/:locale/:electionSlug",
       component: Welcome,
     },
     {
       name: "BallotTrackerView",
-      path: "/:electionSlug/track/:trackingCode",
+      path: "/:locale/:electionSlug/track/:trackingCode",
       component: BallotTrackerView,
     },
   ],
