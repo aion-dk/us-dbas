02a5b98645922fd40cd6484044c39f20c3df2cd3
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-17 23:01:40 +0100

    Add help page FAQs

--- client/src/views/HelpView.vue
+++ client/src/views/HelpView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CompactHeader from "../components/CompactHeader.vue";
+import ExpandableSection from "../components/ExpandableSection.vue"
 </script>
 
 <template>
@@ -10,6 +11,43 @@ import CompactHeader from "../components/CompactHeader.vue";
       <h2 class="HelpView__Title">{{ $t("views.help.title") }}</h2>
       <p class="HelpView__Info">{{ $t("views.help.info") }}</p>
     </div>
+
+    <ExpandableSection class="HelpView__FAQ">
+      <template v-slot:collapsed>
+        <h3>I'm missing my ballot tracking code</h3>
+      </template>
+
+      <template v-slot:expanded>
+        <h3>I'm missing my ballot tracking code</h3>
+        <p>Hello there...</p>
+      </template>
+    </ExpandableSection>
+
+    <ExpandableSection class="HelpView__FAQ">
+      <template v-slot:collapsed>
+        <h3>About Ballot Statuses</h3>
+      </template>
+
+      <template v-slot:expanded>
+        <h3>About Ballot Statuses</h3>
+
+        <p>
+          <strong>Ballot Received</strong> - Your ballot has been received in the digital ballot box and is awaiting signature verification by local election officials.
+        </p>
+
+        <p>
+          <strong>Ballot Accepted</strong> - Your signature affidavit has been verified and your ballot is accepted for counting.
+        </p>
+
+        <p>
+          <strong>Ballot Not Accepted</strong> - There is a problem with your signature affidavit. Contact your local election official for next steps and to cure your affidavit.
+        </p>
+
+        <p>
+          <strong>Ballot Printed for Counting</strong> - Your ballot has been extracted from the digital ballot box and decrypted offline. It has been printed onto a paper ballot for scanning and tabulation with other absentee ballots.
+        </p>
+      </template>
+    </ExpandableSection>
   </main>
 </template>
 
@@ -26,5 +64,10 @@ import CompactHeader from "../components/CompactHeader.vue";
 .HelpView__Info {
   font-weight: 400;
   font-size: 16px;
+}
+
+.HelpView__FAQ h3 {
+  margin: 0;
+  padding: 0;
 }
 </style>
