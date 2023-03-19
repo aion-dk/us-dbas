5c541d0fba1f4f7f17df6ab1c2b24a00ae2d5cce
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-19 05:15:14 +0100

    Test expandable section component

---
+++ client/src/components/ExpandableSection.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "vitest";
+import { mount } from "@vue/test-utils";
+import ExpandableSection from "./ExpandableSection.vue";
+
+const props = { expanded: false }
+const slots = {
+  collapsed: "Collapsed text",
+  expanded: "Expanded text",
+}
+const options = { props, slots }
+
+test("displays the collapsed text", async () => {
+  const wrapper = mount(ExpandableSection, options);
+  expect(wrapper.text()).toContain("Collapsed text");
+});
+
+test("displays the expanded text", async () => {
+  props.expanded = true
+  const wrapper = mount(ExpandableSection, options);
+  expect(wrapper.text()).toContain("Expanded text");
+});
