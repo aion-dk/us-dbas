5aff02cc6ea45b783a2554b53cb2d9fdb7a6497b
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-19 05:12:31 +0100

    Test datetime component

---
+++ client/src/components/DateTime.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import { mount } from "@vue/test-utils";
+import DateTime from "./DateTime.vue";
+
+const props = {
+  dateTime: (new Date()).toISOString(),
+}
+const options = { props }
+
+test("displays relative time", async () => {
+  const wrapper = mount(DateTime, options);
+  expect(wrapper.text()).toContain("a few seconds ago");
+});
+
+test("displays absolute time", async () => {
+  props.format = "absolute"
+  props.dateTime = "2023-01-01T10:00:00+0100"
+  const wrapper = mount(DateTime, options);
+  expect(wrapper.text()).toContain("Sunday, January 1, 2023 10:00 AM CET");
+});
