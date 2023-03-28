85ebdf9ea32fc75f537bb0a7b4cf88464fea645b
Sebastian Berendt <97941073+av-sebastian@users.noreply.github.com>
2023-03-28 13:58:24 +0200

    Update config.ts

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,5 +1,5 @@
 export default {
-  logoUrl: import.meta.env.LOGO_URL || "https://bit.ly/3Ft20ez",
-  dbbUrl: import.meta.env.DBB_URL || "http://dbb:3003",
-  contactUrl: import.meta.env.CONTACT_URL || "https://google.com",
+  logoUrl: import.meta.env.VITE_LOGO_URL || "https://bit.ly/3Ft20ez",
+  dbbUrl: import.meta.env.VITE_DBB_URL || "http://dbb:3003",
+  contactUrl: import.meta.env.VITE_CONTACT_URL || "https://google.com",
 };
