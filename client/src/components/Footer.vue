07c7a9cc4bd414c9d6433280bdfb05c527d32210
av-alexistoledo <alexis.toledo@assemblyvoting.com>
2023-07-13 15:55:07 +0200

    footer img

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -4,7 +4,7 @@
       <span class="Footer__Text">Technology by</span>
       <img
         class="Footer__Image"
-        src="https://s3-alpha-sig.figma.com/img/3286/65ed/aba97ebfe8b64e1207282fe003f1931f?Expires=1679875200&Signature=Xo9hK~xacO6wIPt-nnpcywi7cskam~S-jHxkemuQRicUndLmNuUAx0ZO-afnoaLq8KOaYZMSsZ16Aoc~vYJvu6NYeB~oVogr4Vv7hqiEieEjhjxpCFm6l~QGYPveJO8zXS3VDJWw7lg0eWP4mf5Ugci8eqd646EMy~l9j-l2W4eKF414DhvxKNKlD5daqMleAs~4PZAUxJCy0~nTo0nL3b4RCFoZ6AobjiXbCNMZZtPR01fei9A~ckeb4HhrN6GN7sAZltDf71v~aeOfvCvODlMxU1t8drd75pCm0hDDt~LmDkp-CTDeE5SCDNApHS~MQd0SQFBCc6getEuwXkqUIA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        src="../assets/img/avlogo.png"
         alt="Assembly Voting Logo"
       />
     </a>
@@ -19,7 +19,7 @@
   align-items: center;
   justify-content: center;
   line-height: 20px;
-  padding: 10px;
+  height: 40px;
   font-weight: 400;
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
   position: fixed;
@@ -40,9 +40,8 @@
 }
 
 .Footer__Image {
-  max-height: 20px;
-  margin-left: 4px;
-  width: 125px;
-  height: 20px;
+  max-height: 24px;
+  object-fit: cover;
+  margin-left: 6px;
 }
 </style>
