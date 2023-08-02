8b478e13f74d01a1195ad3df2501e6ddaf468b7d
av-alexistoledo <alexis.toledo@assemblyvoting.com>
2023-08-02 10:12:13 +0200

    spanish translations for header

--- client/src/assets/locales/es.ts
+++ client/src/assets/locales/es.ts
@@ -4,6 +4,20 @@ export const es = {
     es: "Español",
   },
   header: {
+    dbas: "VoteHub Verificación de Boleta",
     about: "Información",
+    logs: {
+      logs: "Registro",
+      config: "Registro de configuraciones",
+      activity: "Registro de actividades",
+    },
+    help: "Ayuda",
+    contact: "Contacto",
+    check: "Chequeo de boleta",
+    track: "Seguimiento de boleta",
+    change_locale: {
+      en: "Switch to English",
+      es: "Cambiar a Español",
+    },
   },
 };
