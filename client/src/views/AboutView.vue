b79627a9e3e90ab1587d5d70278ecbea7ba475dc
av-alexistoledo <112619860+av-alexistoledo@users.noreply.github.com>
2023-08-04 18:09:41 +0200

    [TECH-2245] Change about tab text (#15)
    ![Screenshot 2023-08-03 at 16 16
    07](https://github.com/aion-dk/us-dbas/assets/112619860/d46b8138-db65-47d2-935b-3964147c36a5)

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,37 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import config from "../lib/config";
+</script>
 
 <template>
   <main class="AboutView">
-    <h1>{{ $t("views.about.title") }}</h1>
-
-    <div v-html="$t('views.about.content')"></div>
+    <h1>{{ $t("views.about.content.main_title") }}</h1>
+    <p>{{ $t("views.about.content.main_description") }}</p>
+    <hr />
+    <h2>{{ $t("views.about.content.ballot_check_title") }}</h2>
+    <p>
+      {{ $t("views.about.content.ballot_check_description") }}
+      <a :href="config.securityFeaturesUrl" target="_blank">{{
+        $t("views.about.content.ballot_check_link")
+      }}</a
+      >.
+    </p>
+    <h2>{{ $t("views.about.content.ballot_track_title") }}</h2>
+    <p>{{ $t("views.about.content.ballot_track_description") }}</p>
+    <h2>{{ $t("views.about.content.activity_log_title") }}</h2>
+    <p>{{ $t("views.about.content.activity_log_description") }}</p>
+    <h2>{{ $t("views.about.content.configuration_log_title") }}</h2>
+    <p>{{ $t("views.about.content.configuration_log_description") }}</p>
   </main>
 </template>
 
 <style type="text/css">
 .AboutView {
-  max-width: 800px;
-  margin: auto;
-  font-family: "Open Sans";
+  padding-top: 6rem !important;
+  width: 60vw !important;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
 .AboutView h1 {
   text-align: center;
-  color: var(--neutrals-g-800, #343a40);
-  text-align: center;
+  color: var(--slate-700);
   font-size: 26px;
-  font-family: Open Sans;
-  font-style: normal;
   font-weight: 600;
   line-height: 30px;
 }
 
 .AboutView h2 {
-  color: var(--neutrals-g-800, #343a40);
+  color: var(--slate-700);
   font-size: 24px;
-  font-family: Open Sans;
-  font-style: normal;
   font-weight: 600;
   line-height: 28px;
+}
+
+.AboutView hr {
+  border: 1px solid var(--slate-300);
+  border-radius: 999px;
+  margin: 2rem 0;
+}
+
+.AboutView a {
+  color: var(--slate-900);
+  font-weight: 600;
 }
 </style>
