0df6bb54c2bb8da2ef12b2b80c86b253bcbef6b7
av-alexistoledo <alexis.toledo@assemblyvoting.com>
2023-12-06 10:42:33 +0100

    header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref, onMounted, onBeforeUnmount } from "vue";
 import { useRoute, RouterLink } from "vue-router";
 import config from "../lib/config";
 import DropDown from "./DropDown.vue";
@@ -50,117 +50,120 @@ function classes(name: string) {
       (routeGroups[name] || []).indexOf(route.name.toString()) >= 0,
   };
 }
+
+const isMenuOpened = ref<boolean>(false);
+const isMobile = ref<boolean>(false);
+
+const toggleMenu = () => {
+  isMenuOpened.value = !isMenuOpened.value;
+};
+
+const onResize = (e: Event) => {
+  isMenuOpened.value = false
+  isMobile.value = (e.target as Window).innerWidth <= 1280;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", onResize);
+  isMobile.value = window.innerWidth <= 1280;
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", onResize);
+});
 </script>
 
 <template>
-  <AVNavbar>
-    <div class="Header__logo-section">
-      <img
-        class="Header__Logo"
-        aria-hidden="true"
-        :src="config.logoUrl"
-        alt="Jurisdiction Seal"
-      />
+  <AVNavbar class="Header__Navbar_Overrides">
+    <divc class="Header__logo-section">
+      <img class="Header__Logo" aria-hidden="true" :src="config.logoUrl" alt="Jurisdiction Seal" />
 
-      <RouterLink
-        class="Header__Title"
-        :to="`/${locale}/${election.slug}`"
-        v-if="displayElectionName"
-      >
+      <RouterLink class="Header__Title" :to="`/${locale}/${election.slug}`" v-if="displayElectionName" @click="toggleMenu">
         <span>{{ $t("header.dbas") }}</span>
         <span class="sub">{{ name }}</span>
       </RouterLink>
-    </div>
+    </divc>
 
-    <div class="Header__Links">
-      <RouterLink
-        role="menuitem"
-        :class="classes('verify')"
-        :to="{ name: 'BallotVerifierStart' }"
-      >
+    <div class="Header__Links" :class="{
+      Header__Show: !isMenuOpened,
+    }">
+      <RouterLink role="menuitem" :class="classes('verify')" :to="{ name: 'BallotVerifierStart' }" @click="toggleMenu">
         <span>{{ $t("header.check") }}</span>
       </RouterLink>
 
-      <RouterLink
-        role="menuitem"
-        :class="classes('track')"
-        :to="{ name: 'BallotTrackerStart' }"
-      >
+      <RouterLink role="menuitem" :class="classes('track')" :to="{ name: 'BallotTrackerStart' }" @click="toggleMenu">
         <span>{{ $t("header.track") }}</span>
       </RouterLink>
 
-      <RouterLink
-        id="logs"
-        role="menuitem"
-        :class="classes('logs')"
-        :to="{ name: 'LogsView', params: { type: 'config', page: 1 } }"
-      >
+      <RouterLink v-if="!isMobile" id="logs" role="menuitem" :class="classes('logs')"
+        :to="{ name: 'LogsView', params: { type: 'config', page: 1 } }">
         <span>{{ $t("header.logs.logs") }}</span>
         <ul id="logs-submenu">
           <li>
-            <RouterLink
-              :to="{ name: 'LogsView', params: { type: 'activity', page: 1 } }"
-              >{{ $t("header.logs.activity") }}</RouterLink
-            >
+            <RouterLink :to="{ name: 'LogsView', params: { type: 'activity', page: 1 } }">{{ $t("header.logs.activity") }}
+            </RouterLink>
           </li>
           <li>
-            <RouterLink
-              :to="{ name: 'LogsView', params: { type: 'config', page: 1 } }"
-              >{{ $t("header.logs.config") }}</RouterLink
-            >
+            <RouterLink :to="{ name: 'LogsView', params: { type: 'config', page: 1 } }">{{ $t("header.logs.config") }}
+            </RouterLink>
           </li>
         </ul>
       </RouterLink>
 
-      <RouterLink
-        :class="classes('AboutView')"
-        role="menuitem"
-        :to="{ name: 'AboutView' }"
-      >
+      <RouterLink v-if="isMobile" :class="classes('logs')" :to="{ name: 'LogsView', params: { type: 'activity', page: 1 } }" @click="toggleMenu">{{
+        $t("header.logs.activity") }}</RouterLink>
+
+      <RouterLink v-if="isMobile" :class="classes('logs')" :to="{ name: 'LogsView', params: { type: 'config', page: 1 } }" @click="toggleMenu">{{
+        $t("header.logs.config") }}</RouterLink>
+
+      <RouterLink :class="classes('AboutView')" role="menuitem" :to="{ name: 'AboutView' }" @click="toggleMenu">
         <span>{{ $t("header.about") }}</span>
       </RouterLink>
 
-      <RouterLink
-        role="menuitem"
-        :class="classes('HelpView')"
-        :to="{ name: 'HelpView' }"
-      >
+      <RouterLink role="menuitem" :class="classes('HelpView')" :to="{ name: 'HelpView' }" @click="toggleMenu">
         <span>{{ $t("header.help") }}</span>
       </RouterLink>
 
-      <a
-        role="menuitem"
-        class="Header__Link"
-        :href="config.contactUrl"
-        target="_blank"
-      >
+      <a role="menuitem" class="Header__Link" :href="config.contactUrl" target="_blank" @click="toggleMenu">
         <span>
           <AVIcon icon="arrow-up-right-from-square" aria-hidden="true" />
           {{ $t("header.contact") }}
         </span>
       </a>
 
-      <DropDown
-        class="Header__Locales"
-        :options="availableLocales"
-        @change="(value) => setLocale(value)"
-      />
+      <DropDown class="Header__Locales" :options="availableLocales" @change="(value) => setLocale(value)" />
     </div>
+
+    <button class="Header__Hamburger_Btn" :aria-label="isMenuOpened
+        ? $t('header.close_menu_aria_label')
+        : $t('header.open_menu_aria_label')
+      " @click="toggleMenu">
+      <AVIcon v-if="isMenuOpened" icon="xmark" class="Header__Hamburger_Icon" aria-hidden="true" />
+      <AVIcon v-else icon="bars" class="Header__Hamburger_Icon" aria-hidden="true" />
+    </button>
   </AVNavbar>
 </template>
 
 <style type="text/css" scoped>
+.Header__Navbar_Overrides {
+  padding: .5rem !important;
+}
+
 .Header__logo-section {
   display: flex;
-  justify-content: center;
   align-items: center;
+}
+
+.Header__Logo {
+  height: 54px;
+  width: 54px;
+  object-fit: cover;
 }
 
 .Header__Title {
   font-weight: 600;
-  font-size: 22px;
-  margin: 0;
-  padding: 0;
+  font-size: 16px;
+  margin-left: 1rem;
   text-decoration: none;
   color: var(--slate-700);
   display: flex;
@@ -168,82 +171,156 @@ function classes(name: string) {
 }
 
 .Header__Title .sub {
-  font-size: 16px;
-}
-
-.Header__Logo {
-  height: 54px;
-  width: 54px;
-  object-fit: cover;
-  margin: 8px 11px;
-  border-radius: 3px;
+  font-size: 12px;
 }
 
 .Header__Links {
-  flex-grow: 1;
-  justify-content: flex-end;
-  align-items: center;
+  position: absolute;
+  top: 70px;
+  left: 0px;
   display: flex;
-  gap: 12px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: calc(100dvh - 70px);
+  padding-bottom: 3rem;
+  background-color: white;
+}
+
+.Header__Show {
+  display: none;
+}
+
+.Header__Hamburger_Btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background: none;
+  font-size: 1.5rem;
+  height: 54px;
+  width: 54px;
+  color: var(--slate-800);
 }
 
 .Header__Link {
-  padding: 8px 16px;
-  font-size: 18px;
-  font-weight: 700;
-  text-transform: uppercase;
+  padding: 1.25rem;
+  font-size: 1.125rem;
+  font-weight: 400;
   text-decoration: none;
   color: var(--slate-700);
-  border-radius: 12px;
-  border: transparent 2px solid;
+}
+
+.Header__Link:hover {
+  color: var(--slate-900);
 }
 
 .Header__Link:hover:not(.router-link-active) {
   background-color: var(--slate-100);
 }
 
-ul > li > .router-link-active {
-  border: none;
-}
-
-.router-link-active:not(.Header__Title) {
-  border-color: var(--slate-300);
-}
-
 .Header__Locales {
-  margin-right: 20px;
-  font-size: 16px;
-  text-decoration: none;
+  font-size: 1.125rem;
+  font-weight: 400;
   color: var(--slate-700);
   border: none;
-  text-transform: uppercase;
-  font-weight: 700;
-  font-size: 18px;
-  padding: 4px;
-}
-
-#logs-submenu {
-  display: none;
-  position: absolute;
-  list-style: none;
-  padding: 0;
-  margin: 12px 0 0 -16px;
   background-color: white;
-  box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.15);
+  padding: 1rem 0 1rem 1rem;
 }
 
-#logs-submenu a:hover {
-  background-color: var(--slate-100);
-}
+@media only screen and (min-width: 80rem) {
+  .Header__Navbar_Overrides {
+    padding-right: 1.5rem !important;
+    padding-left: 1rem !important;
+  }
 
-#logs-submenu a {
-  display: block;
-  padding: 16px 20px;
-  text-decoration: none;
-  color: var(--slate-700);
-}
+  .Header__Hamburger_Btn {
+    display: none;
+  }
 
-#logs:hover #logs-submenu {
-  display: block;
+  .Header__Title {
+    font-size: 22px;
+  }
+
+  .Header__Title .sub {
+    font-size: 16px;
+  }
+
+  .Header__Links {
+    display: block;
+    position: static;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    width: fit-content;
+    height: fit-content;
+    text-wrap: nowrap;
+    padding-bottom: 0;
+    flex-grow: 1;
+    gap: 1rem;
+  }
+
+  .Header__Link {
+    padding: 8px 16px;
+    font-size: 18px;
+    font-weight: 700;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: var(--slate-700);
+    border-radius: 12px;
+    border: transparent 2px solid;
+  }
+
+  .Header__Link:hover:not(.router-link-active) {
+    background-color: var(--slate-100);
+  }
+
+  ul>li>.router-link-active {
+    border: none;
+  }
+
+  .router-link-active:not(.Header__Title) {
+    border-color: var(--slate-300);
+  }
+
+  .Header__Locales {
+    margin-right: 20px;
+    font-size: 16px;
+    text-decoration: none;
+    color: var(--slate-700);
+    border: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 18px;
+    padding: 4px;
+  }
+
+  #logs-submenu {
+    display: none;
+    position: absolute;
+    list-style: none;
+    padding: 0;
+    margin: 12px 0 0 -16px;
+    background-color: white;
+    box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.15);
+  }
+
+  #logs-submenu a:hover {
+    background-color: var(--slate-100);
+  }
+
+  #logs-submenu a {
+    display: block;
+    padding: 16px 20px;
+    text-decoration: none;
+    color: var(--slate-700);
+  }
+
+  #logs:hover #logs-submenu {
+    display: block;
+  }
+
 }
 </style>
